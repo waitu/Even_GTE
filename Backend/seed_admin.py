ee7c0e23b58c363005bff6d10e338db9c4c3b196
Giang Lê <giangvipbg2002@gmail.com>
2026-01-05 18:35:30 +0700

    main /api

--- Backend/seed_admin.py
+++ Backend/seed_admin.py
@@ -6,7 +6,7 @@ from app.core.security import get_password_hash
 def seed_admin():
     db = SessionLocal()
     if not db.query(User).filter(User.username == "admin").first():
-        admin = User(username="admin", hashed_password=get_password_hash("admin123"), is_admin=True)
+        admin = User(username="admin", hashed_password=get_password_hash("#c395e97c9b"), is_admin=True)
         db.add(admin)
         db.commit()
     db.close()
